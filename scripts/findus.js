30b10d731390414791e6331245251cb936d041d2
Dennis Collins <donncha.ocoileain@hotmail.com>
2025-03-19 13:09:00 +0100

    Kommentarer till findus.js

--- scripts/findus.js
+++ scripts/findus.js
@@ -12,18 +12,22 @@ document.addEventListener("DOMContentLoaded", function () {
     "lördag",
   ];
 
+  // Beräkna vilken veckodag det är.
   const currentDayIndex = today.getDay();
 
+  // Tar vekodagsnamn från arrayen.
   const day3 = weekdays[(currentDayIndex + 2) % 7];
   const day4 = weekdays[(currentDayIndex + 3) % 7];
   const day5 = weekdays[(currentDayIndex + 4) % 7];
 
+  // Ersätter placeholder text i HTML med veckodag.
   document.getElementById("day-1").textContent = "idag";
   document.getElementById("day-2").textContent = "i morgon";
   document.getElementById("day-3").textContent = day3;
   document.getElementById("day-4").textContent = day4;
   document.getElementById("day-5").textContent = day5;
 
+  // Beräknar tiden nu i Sverige.
   const now = new Date();
   const stockholmTime = new Intl.DateTimeFormat("sv-SE", {
     timeZone: "Europe/Stockholm",
@@ -36,11 +40,13 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentHour = 0;
   let currentDay = "";
 
+  // Parse "hours" string till siffror.
   stockholmTime.forEach((part) => {
     if (part.type === "hour") currentHour = parseInt(part.value);
     if (part.type === "weekday") currentDay = part.value;
   });
 
+  // Öppettiderna
   const openHours = {
     söndag: { start: 11, end: 20 },
     måndag: { start: 11, end: 20 },
@@ -51,10 +57,12 @@ document.addEventListener("DOMContentLoaded", function () {
     lördag: { start: 11, end: 23 },
   };
 
+  // Beräknar om aktuell tid är mellan öppettiderna
   const isOpen =
     currentHour >= openHours[currentDay].start &&
     currentHour < openHours[currentDay].end;
 
+  // Visar/gömmer kartan baserat på tiden. Ersätter kartan med text om stängt.
   const iframe = document.querySelector(".find__map-frame iframe");
   const closedMessage = document.getElementById("closed-message");
 
